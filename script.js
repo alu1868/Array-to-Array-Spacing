345fdd52dd38e461bfd3f1ee4ed568c1f98fe615
Alex <alu1868@gmail.com>
2022-10-04 14:58:18 -0500

    calculating height difference and array spacing

--- script.js
+++ script.js
@@ -4,19 +4,56 @@
 // spacing between Modules in array
 // Array tilt angle
 // Solar Elevation Angle
-function RetreiveInputs() {}
+function RetreiveInputs() {
+  // Array + Module Info
+  var ModuleWidthId = document.getElementById("ModuleWidth")
+  var RowsInArrayId = document.getElemenyById("NumberOfRows")
+  var RowGapId = document.getElemenyById("RowGap")
+  var ArrayTiltId = document.getElementById("ArrayTilt")
+
+  var ZipCodeId =  document.getElementById("ZipCode")
+  var SolarElevationAngleId = document.getElementById("solarElevationAngle")
+
+  var ArrayWidthSum =  (ModuleWidthId * RowsInArrayId) + (RowGapId * (RowsInArrayId - 1))
+  var HeightDifference = (HeightDifferenceCalc(ArrayTiltId, ArrayWidthSum))
+  var ArraySpacing = (ArraySpacingCalc(HeightDifference, SolarElevationAngleId))
+}
 
 // 2. Calculate for HeightDifference
-function HeightDifferenceCalc() {}
+function HeightDifferenceCalc(ArrayTiltId, ArrayWidthSum) {
+  var ConvertToRadians = (ArrayTiltId * Math.PI)/180
+  var HeightDifference = Math.sin(ConvertToRadians) * ArrayWidthSum
+  return Math.round(HeightDifference)
+}
 
-// 3. Rounding function (to nearest inch)
-function Rounding() {}
+// 3. Calculate for Array Spacing
+function ArraySpacingCalc(HeightDifference, SolarElevationAngle) {
+  var ArraySpacing = HeightDifference / Math.tan(SolarElevationAngle)
+  return Math.round(ArraySpacing)
+}
 
-// 4. Calculate for Module Row Spacing
-function ModuleRowSpacingCalc() {}
+// 4. Calculate for Minimum Array Row Spacing
+function MinimumArrayRowSpacingCalc(ArraySpacing, AzimuthCorrectionAngle) {
+  // MEASUREMENT FOR SPACE BETWEEN ARRAYS
+  var MinimumArrayRowSpacing =  ArraySpacing * Math.cos(AzimuthCorrectionAngle)
+}
 
-// 5. Calculate for Minimum Module Row Spacing
-function MinimumModuleRowSpacingCalc() {}
+// 5. Calculate for Row Width
+function RowWidthCalc() {
 
-// 6. Calculate for Row Width
-function RowWidthCalc() {}
+}
+
+function SaveModuleLocal() {}
+
+function DeployModuleArray() {}
+
+function DeleteModuleLocal() {}
+
+
+// TEST
+function getsinx (degrees) {
+  var Radians = (degrees * Math.PI)/180
+	return Math.sin(Radians)
+};
+
+console.log(getsinx(25))
