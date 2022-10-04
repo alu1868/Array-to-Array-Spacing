2b24b85420fa35813d48fc93bf2c20657112829b
Alex <alu1868@gmail.com>
2022-10-04 13:28:31 -0500

    added outline for spacing calculations + outline for visual aid on html

---
+++ script.js
@@ -0,0 +1,22 @@
+// 1. Retreive user inputs about module information
+// looking for module width (Landscape orientation)
+// number of rows in Array
+// spacing between Modules in array
+// Array tilt angle
+// Solar Elevation Angle
+function RetreiveInputs() {}
+
+// 2. Calculate for HeightDifference
+function HeightDifferenceCalc() {}
+
+// 3. Rounding function (to nearest inch)
+function Rounding() {}
+
+// 4. Calculate for Module Row Spacing
+function ModuleRowSpacingCalc() {}
+
+// 5. Calculate for Minimum Module Row Spacing
+function MinimumModuleRowSpacingCalc() {}
+
+// 6. Calculate for Row Width
+function RowWidthCalc() {}
