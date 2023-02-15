a4a14d77db993419a9edc8c50d76d68b003fba02
Alex <alu1868@gmail.com>
2023-02-14 18:40:16 -0600

    added required form inputs

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// DOM Collection
+var formEl = document.querySelector("#user-form")
+
+
+// Capture the Form
+var formHandler = function(event) {
+  event.preventDefault();
+
+  // Grab Form Values
+  var moduleLength = document.querySelector("input[name='module-length']").value
+  var moduleWidth = document.querySelector("input[name='module-width']").value
+  var numberOfRows = document.querySelector("input[name='number-of-rows']").value
+  var arrayTilt = document.querySelector("input[name='array-tilt']").value
+  var zipCode = document.querySelector("input[name='zip-code']").value
+
+  console.log("module length: " + moduleLength)
+  console.log("module width: " + moduleWidth)
+  console.log("number of rows: " + numberOfRows)
+  console.log("array tilt: " + arrayTilt)
+  console.log("zipcode: " + zipCode)
+}
+
+// Grab Solar Elevation Angle 
+
+// Grab Azimuth Correction Angle
+
+// Calculate Module Row Spacing
+
+// Calculate Height Difference
+
+// Calculate Row Width
+
+formEl.addEventListener("submit", formHandler)
